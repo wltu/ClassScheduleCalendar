476c398304b48e789a170c1b0f530a48ac244dfd
Willy Tu <tu.willy.97@gmail.com>
2020-06-16 10:41:16 -0700

    Added more unit test

--- ClassScheduleCalendar/ClassScheduleCalendar/Course.cs
+++ ClassScheduleCalendar/ClassScheduleCalendar/Course.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassScheduleCalendar
 {
-    class Course
+    public class Course
     {
         public List<Class> Classes { get; set; }
 
